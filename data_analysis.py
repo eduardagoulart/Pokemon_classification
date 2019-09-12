f7f6b21e7a0317c863530f1193bfee178206d230
eduardagoulart <goularteduarda.a@gmail.com>
2019-09-12 20:59:36 -0300

    FEATURE: analysis of agaisnt bug -> now do the same for the others against features

--- data_analysis.py
+++ data_analysis.py
@@ -1,9 +1,48 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 df = pd.read_csv('pokemon.csv')
 df = df.drop(
-    ['japanese_name', 'percentage_male', 'pokedex_number', 'generation', 'height_m', 'base_egg_steps', 'weight_kg'],
-    axis=1)
-df.to_csv("filtering.csv")
+    ['japanese_name', 'percentage_male', 'pokedex_number', 'generation', 'height_m', 'base_egg_steps', 'weight_kg',
+     'experience_growth', 'speed', 'base_happiness', 'base_total', 'is_legendary', 'sp_defense', 'sp_attack', 'name',
+     'hp', "defense", 'abilities', 'attack', 'capture_rate'], axis=1)
+
 print(df)
-print(df.describe())
+df.to_csv('filtering.csv')
+sns.set(color_codes=True)
+
+
+def types_corr():
+    bug = [df[df.type1 == 'bug']]
+    pearson_corr = df.corr(method='pearson')
+    spearman_corr = df.corr(method='spearman')
+    kendall_corr = df.corr(method='kendall')
+    pearson_corr.to_csv('corr.csv')
+
+
+def against_bug_type1_count():
+    """
+    How against bug distribution with all type1 class
+    :return: graph
+    """
+    type1 = df['type1'].drop_duplicates().tolist()
+    type1_count = df.groupby("type1").count()
+    type1_count['type1'] = type1
+    type1_count.plot(kind='bar', x='type1', y='against_bug')
+    plt.show()
+
+
+def against_bug_distribution():
+    x = np.array(df['against_bug'])
+    bins_vec = []
+    for i in x:
+        if i not in bins_vec:
+            bins_vec.append(i)
+
+    bins_vec = sorted(bins_vec)
+    plt.hist(x, bins=bins_vec)
+    plt.show()
+
+
