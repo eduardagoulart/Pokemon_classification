b0ed0268a47ecb2e34b707b0c4c30abacc534c4b
eduardagoulart <goularteduarda.a@gmail.com>
2019-10-12 14:15:08 -0300

    update m-estimate

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 import pandas as pd
 import operator
 
-inputs = [
+inputs = [  # Esse input vai ser dado pelo usuário, apenas um caso teste
+    1.0,
+    1.0,
     1.0,
     0.5,
-    1.0,
+    0.5,
+    0.5,
     2.0,
-    0.5,
-    0.5,
-    1.0,
-    1.0,
-    1.0,
-    1.0,
     2.0,
-    0.5,
+    1.0,
+    0.25,
     1.0,
     2.0,
     1.0,
+    1.0,
     2.0,
+    1.0,
     1.0,
     0.5,
 ]
@@ -44,9 +44,7 @@ against = [
 ]
 
 df = pd.read_csv("pokemon.csv")
-df_len = len(df)
 type_frequency = df.type1.value_counts()
-
 
 df_class = {
     "water": type_frequency[0],
@@ -69,11 +67,43 @@ df_class = {
     "flying": type_frequency[17],
 }
 
-probability_class = {}
-for type1, freq in df_class.items():
-    probability_class[type1] = freq / df_len
+hash_change_value = {
+    "against_bug": "bug",
+    "against_dark": "dark",
+    "against_dragon": "dragon",
+    "against_electric": "electric",
+    "against_fairy": "fairy",
+    "against_fight": "fight",
+    "against_fire": "fire",
+    "against_flying": "flying",
+    "against_ghost": "ghost",
+    "against_grass": "grass",
+    "against_ground": "ground",
+    "against_ice": "ice",
+    "against_normal": "normal",
+    "against_poison": "poison",
+    "against_psychic": "psychic",
+    "against_rock": "rock",
+    "against_steel": "steel",
+    "against_water": "water",
+}
 
 
+def class_probability(_class):
+    return df_class[_class] / len(df)
+
+
+def probability_elem(feature, value, class_value, p_class, m):
+    n = len(df[df[feature] == value])
+    n_c = len(df[(df.type1 == class_value) & (df[feature] == value)])
+    return (n_c + (p_class * m)) / (m + n)
+    # print(df[df[feature] == value])
+
+
+info = hash_change_value[against[0]]
+# print(df_class[info])
+print(probability_elem(against[0], inputs[0], info, class_probability(info), m=5))
+"""
 probability = {}
 for key, item in df_class.items():
     prob = []
@@ -92,11 +122,14 @@ for key, item in df_class.items():
     probability[key] = aux
 
 print(probability)
-sorted_d = dict( sorted(probability.items(), key=operator.itemgetter(0),reverse=True))
+sorted_d = dict(sorted(probability.items(), key=operator.itemgetter(0)))
+print(sorted_d)
 result = list(sorted_d.keys())[0]
+print(probability[result])
 final_prob = (probability[result] / sum(probability.values())) * 100
 print(f"Probabilidade de {final_prob} de ser {result}")
 
 # value1 = len(new_df)
 # value2 = df_class["water"]
 # print(f"probability = {value1 / value2}")
+"""
