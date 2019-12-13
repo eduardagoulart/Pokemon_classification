b5ad5a725eb85947f97c2b3743741d97b611cc8c
Eduarda Goulart <goularteduarda.a@gmail.com>
2019-12-13 19:46:20 -0300

    values with low frequency

--- improviments_nb.py
+++ improviments_nb.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, confusion_matrix
@@ -6,6 +7,34 @@ from sklearn.cluster import KMeans
 
 
 df = pd.read_csv("pokemon.csv")
+
+type_frequency = df.type1.value_counts()
+
+
+df_class = {
+    "water": type_frequency[0],
+    "normal": type_frequency[1],
+    "grass": type_frequency[2],
+    "bug": type_frequency[3],
+    "psychic": type_frequency[4],
+    "fire": type_frequency[5],
+    "rock": type_frequency[6],
+    "electric": type_frequency[7],
+    "ground": type_frequency[8],
+    "poison": type_frequency[9],
+    "dark": type_frequency[10],
+    "fighting": type_frequency[11],
+    "dragon": type_frequency[12],
+    "ghost": type_frequency[13],
+    "steel": type_frequency[14],
+    "ice": type_frequency[15],
+    "fairy": type_frequency[16],
+    "flying": type_frequency[17],
+}
+
+classes = [key for key, value in df_class.items() if value >= 25]
+df = df[df['type1'].isin(classes)]
+
 df_X = df[
     [
         "against_bug",
@@ -31,8 +60,6 @@ df_X = df[
 
 df_Y = df[["type1"]]
 
-kmeans = KMeans(n_clusters=8, random_state=0).fit(df_X)
-
 X_train, X_test, y_train, y_test = train_test_split(
     df_X, df_Y, test_size=0.3, random_state=1
 )
@@ -40,5 +67,4 @@ X_train, X_test, y_train, y_test = train_test_split(
 gnb = GaussianNB()
 y_pred = gnb.fit(X_train, y_train).predict(X_test)
 
-print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
